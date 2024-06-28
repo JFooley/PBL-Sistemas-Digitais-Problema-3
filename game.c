f15db896ac73e56a6ea0d7e3c24e339259cfe40f
João Gabriel <69651705+JFooley@users.noreply.github.com>
2024-06-28 16:50:35 -0300

    update

--- game.c
+++ game.c
@@ -8,8 +8,8 @@
 #include "lib.c"
 
 // Posição do mouse
-int pos_x = 100;
-int pos_y = 100;
+int mouse_pos_x = 100;
+int mouse_pos_y = 100;
 
 // Variáveis globais para armazenar movimentos e cliques do mouse
 int mouse_dx = 0;
@@ -24,6 +24,18 @@ int dyAnterior = 0;
 int botaoEsquerdoAnterior = 0;
 int botaoDireitoAnterior = 0;
 int botaoMeioAnterior = 0;
+
+typedef struct
+{
+    int pos_X;      // Canto superior esquerdo
+    int pos_Y;
+    int length;     // Tamanho em pixels
+    int on_screen;  // Está ou não na tela
+} ColiderBox;
+
+ColiderBox asteroids[10];
+ColiderBox blasts[3];
+ColiderBox nave;
 
 int *monitorarMouse(void *arg)
 {
@@ -55,14 +67,35 @@ int *monitorarMouse(void *arg)
         botaoMeioAnterior = botaoMeio;
         botaoMeio = data[0] & 0x04;
 
-        pos_x += mouse_dx;
-        pos_y -= mouse_dy;
+        mouse_pos_x += mouse_dx;
+        mouse_pos_y -= mouse_dy;
 
     }
 
     close(fd);
     return 0;
 }
+
+int check_colision(ColiderBox costant, ColiderBox optional) {
+    // Calcula as coordenadas dos cantos das caixas
+    int left1 = costant.pos_X;
+    int right1 = costant.pos_X + costant.length;
+    int top1 = costant.pos_Y;
+    int bottom1 = costant.pos_Y + costant.length;
+
+    int left2 = optional.pos_X;
+    int right2 = optional.pos_X + optional.length;
+    int top2 = optional.pos_Y;
+    int bottom2 = optional.pos_Y + optional.length;
+
+    // Verifica se não há colisão
+    if (bottom1 <= top2 || top1 >= bottom2 || right1 <= left2 || left1 >= right2) {
+        return 0;
+    }
+
+    // Há colisão
+    return 1;
+};
 
 int main()
 {
@@ -74,9 +107,19 @@ int main()
         return 1;
     }
 
+    ColiderBox box1 = {100, 100, 20, 1};
+    ColiderBox box2 = {0, 0, 20, 1};
+
     while (1)
     {
-        WBR_S(6, 6, pos_x, pos_y, 1);
+        WBR_S(6, 6, mouse_pos_x, mouse_pos_y, 1);
+        box2.pos_X = mouse_pos_x;
+        box2.pos_Y = mouse_pos_y;
+        
+        WBR_S(1, 1, 100, 100, 1);
+        if (check_colision(box1, box2)) {
+            printf("colidiu");
+        };
     }
 
     pthread_join(threadMouse, NULL);
